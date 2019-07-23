0fb00083c26cc5248f64e673725c476deec54475
Christux <christux57@free.fr>
2019-07-23 23:00:34 +0200

    Add shift register ledstrip example

---
+++ examples/ShiftRegisterLedStrip/ShiftRegisterLedStrip.h
@@ -0,0 +1,118 @@
+/* 
+ * Copyright (c) 2017 Christophe Rubeck.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU Lesser General Public License as   
+ * published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * Lesser General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef ShiftRegisterLedStrip_h
+#define ShiftRegisterLedStrip_h
+
+#include <Arduino.h>
+#include <ChristuxAnimation.h>
+
+namespace ChristuxAnimation {
+
+	class ShiftRegisterLedStrip: public LedStrip
+	{
+	protected:
+		const uint8_t _N;
+		uint8_t *_T;
+		const uint8_t _clockPin;
+		const uint8_t _latchPin;
+		const uint8_t _dataPin;
+		const uint8_t CLOCK_TEMP = 50; // u-seconds
+		
+	public:
+		ShiftRegisterLedStrip(uint8_t nLeds, uint8_t clockPin, uint8_t latchPin, uint8_t dataPin);
+		~ShiftRegisterLedStrip();
+		void Begin();
+		void Show();
+		void SetPixelColor(int, RgbColor);
+        void SetAllPixels(RgbColor);
+		
+	private:
+		inline void putch() const;
+		inline void clock() const;
+		inline void latch() const;
+	};
+
+	/*
+	* Construtor
+	*/
+	ShiftRegisterLedStrip::ShiftRegisterLedStrip(uint8_t nLeds, uint8_t clockPin, uint8_t latchPin, uint8_t dataPin) :
+		_N(nLeds),
+		_T((uint8_t*)malloc(_N)),
+		_clockPin(clockPin),
+		_latchPin(latchPin),
+		_dataPin(dataPin)
+	{}
+
+	ShiftRegisterLedStrip::~ShiftRegisterLedStrip() {
+		free(_T);
+	}
+
+	/*
+	* Setup transmission pins
+	*/
+	void ShiftRegisterLedStrip::Begin() {
+		pinMode(_latchPin, OUTPUT);
+		pinMode(_clockPin, OUTPUT);
+		pinMode(_dataPin, OUTPUT);
+	}
+
+	/*
+	* Sets value of the i-th led
+	*/
+	void ShiftRegisterLedStrip::SetPixelColor(int i, RgbColor color) {
+		_T[i] = color.CalculateBrightness() > 64 ? 1 : 0;
+	}
+
+	void ShiftRegisterLedStrip::SetAllPixels(RgbColor color) {
+		for (int i = 0; i < _N; i++) {
+			SetPixelColor(i, color);
+		}
+	}
+
+	/*
+	* Commits led states to the physical led strip
+	*/
+	void ShiftRegisterLedStrip::Show() {
+		for (int i = 0; i < _N; i++) {
+			digitalWrite(_dataPin, _T[_N - i - 1]);
+			clock();
+		}
+		latch();
+	}
+
+	/*
+	* Transmission methods, see data sheet of your shiftregister
+	*/
+	inline void ShiftRegisterLedStrip::putch() const {
+		delayMicroseconds(CLOCK_TEMP);
+	}
+
+	inline void ShiftRegisterLedStrip::clock() const {
+		digitalWrite(_clockPin, HIGH);
+		putch();
+		digitalWrite(_clockPin, LOW);
+		putch();
+	}
+
+	inline void ShiftRegisterLedStrip::latch() const {
+		digitalWrite(_latchPin, HIGH);
+		putch();
+		digitalWrite(_latchPin, LOW);
+		putch();
+	}
+}
+#endif
---
+++ examples/ShiftRegisterLedStrip/ShiftRegisterLedStrip.ino
@@ -0,0 +1,73 @@
+/*
+   Copyright (c) 2017 Christophe Rubeck.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, version 3.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Lesser General Lesser Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define animationDemo
+#include <ChristuxAnimation.h>
+#include "ShiftRegisterLedStrip.h"
+
+#define LATCH_PIN 9
+#define CLOCK_PIN 8
+#define DATA_PIN 10
+
+using namespace ChristuxAnimation;
+
+uint8_t PixelCount = 8;
+
+ShiftRegisterLedStrip strip = ShiftRegisterLedStrip(PixelCount, CLOCK_PIN, LATCH_PIN, DATA_PIN);
+
+Animator animator;
+
+Comet comet(PixelCount, &strip);
+Theater theater(PixelCount, &strip);
+KnightRider rider(PixelCount, &strip, 100);
+Blink blink(PixelCount, &strip, 1000);
+
+// For animation loop
+const char * labels[] = {"Rainbow","Sunrise","Unicolor","Flag","Comet","Theater","Knight Rider","Rainbow lamp","Random rainbow lamp","Fire","Breathing","Blink"};
+unsigned long nextFlicker = 0;
+unsigned long temp = 6000; // Animation duration
+int step = 0;
+int numStep;
+
+void setup() {
+
+  Serial.begin(9600);
+
+  strip.Begin();
+
+  animator.add(&comet);
+  animator.add(&theater);
+  animator.add(&rider);
+  animator.add(&blink);
+
+  // Do not forget to set a color !
+  animator.setColor(ChristuxAnimation::RgbColor::red);
+
+  numStep = animator.animCount();
+}
+
+void loop() {
+
+  unsigned long currTime = millis();
+  if (currTime >= nextFlicker) {
+    step = step < numStep - 1 ? step + 1 : 0;
+    animator.setAnimation(step);
+    Serial.println(labels[step]);
+    nextFlicker = currTime + temp;
+  }
+
+  animator.handle();
+}
