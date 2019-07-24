716cae2fdf0ea8d3cebebca882c73a7ba39c9478
Christux <christux57@free.fr>
2019-07-24 13:11:39 +0200

    Headers update

--- src/Animation/Temporized/KnightRider.cpp
+++ src/Animation/Temporized/KnightRider.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Christophe Rubeck.
+ * Copyright (c) 2019 Christophe Rubeck.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
